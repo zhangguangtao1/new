e2df562c3d3b8d74676b78a5094b67dd0bad038c
张广涛 <1223629810@qq.com>
2020-04-11 22:08:30 +0800

    修改缓存问题

--- shurenhao/plugin/Js-Jquery-master/app.js
+++ shurenhao/plugin/Js-Jquery-master/app.js
@@ -49,16 +49,16 @@
 	Sortable.create(byId('foo'), {
 		group: "words",
 		animation: 150,
-		store: {
-			get: function (sortable) {
-				var order = localStorage.getItem(sortable.options.group);
-				return order ? order.split('|') : [];
-			},
-			set: function (sortable) {
-				var order = sortable.toArray();
-				localStorage.setItem(sortable.options.group, order.join('|'));
-			}
-		},
+		// store: {
+		// 	get: function (sortable) {
+		// 		var order = localStorage.getItem(sortable.options.group);
+		// 		return order ? order.split('|') : [];
+		// 	},
+		// 	set: function (sortable) {
+		// 		var order = sortable.toArray();
+		// 		localStorage.setItem(sortable.options.group, order.join('|'));
+		// 	}
+		// },
 		onAdd: function (evt){ console.log('onAdd.foo:', [evt.item, evt.from]); },
 		onUpdate: function (evt){ console.log('onUpdate.foo:', [evt.item, evt.from]); },
 		onRemove: function (evt){ console.log('onRemove.foo:', [evt.item, evt.from]); },
